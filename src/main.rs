c4f12840fcb30dd57c9fc0764d0ed51c027d042a
Mark Wales <mark.wales@carescribe.io>
2022-10-17 10:48:40 +0100

    add concurrency

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
+extern crate num_cpus;
 
 use num::Complex;
 use std::str::FromStr;
@@ -65,7 +67,7 @@ fn render(pixels: &mut [u8],
         for column in 0 .. bounds.0 {
             let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
 
-            pixels[row * bounds.0 + column] = 
+            pixels[row * bounds.0 + column] =
                 match escape_time(point, 255) {
                     None => 0,
                     Some(count) => 255 - count as u8,
@@ -98,7 +100,27 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = num_cpus::get();
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> =
+            pixels.chunks_mut(rows_per_band * bounds.0).collect();
+
+        crossbeam::scope(| spawner | {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
 
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }

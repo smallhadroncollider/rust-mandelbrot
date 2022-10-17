3b65a6648428ce52fd1edbe49d2c864881542ff1
Mark Wales <mark.wales@carescribe.io>
2022-10-17 12:54:07 +0100

    simplify Option usage

--- src/main.rs
+++ src/main.rs
@@ -25,22 +25,16 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
 }
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
-    match s.find(separator) {
-        None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l, r)),
-                _ => None
-            }
+    s.find(separator).map(| index | {
+        match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None
         }
-    }
+    }).flatten()
 }
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
-    match parse_pair(s, ',') {
-      Some((re, im)) => Some(Complex { re, im } ),
-      None=> None
-    }
+    parse_pair(s, ',').map(|(re, im)| Complex { re, im })
 }
 
 fn pixel_to_point(bounds: (usize, usize),
